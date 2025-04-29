06c651af0c194ad588a7af311210341811b94be8
Balu Tirupati <balusekhar8@gmail.com>
2025-04-28 21:33:53 -0500

    Added Security Method Annotations

--- src/main/java/com/balutirupati/securitydemo/controllers/PostController.java
+++ src/main/java/com/balutirupati/securitydemo/controllers/PostController.java
@@ -2,6 +2,7 @@ package com.balutirupati.securitydemo.controllers;
 
 import com.balutirupati.securitydemo.entities.PostEntity;
 import com.balutirupati.securitydemo.services.PostService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,24 +13,25 @@ import java.util.List;
 @RestController
 public class PostController {
 
-    public final PostService postService;
+  public final PostService postService;
 
-    public PostController(PostService postService) {
-        this.postService = postService;
-    }
+  public PostController(PostService postService) {
+    this.postService = postService;
+  }
 
-    @GetMapping(value = "")
-    public String hello() {
-        return "Hello World!";
-    }
+  @GetMapping(value = "")
+  public String hello() {
+    return "Hello World!";
+  }
 
-    @GetMapping(value = "/posts")
-    public List<PostEntity> getPosts() {
-        return postService.getAllposts();
-    }
+  @GetMapping(value = "/posts")
+  public List<PostEntity> getPosts() {
+    return postService.getAllposts();
+  }
 
-    @PostMapping(value = "/posts")
-    public PostEntity creaatePosts(@RequestBody PostEntity postDetails) {
-        return postService.createpost(postDetails);
-    }
+  @PostMapping(value = "/posts")
+  @PreAuthorize("hasRole('CREATOR')")
+  public PostEntity creaatePosts(@RequestBody PostEntity postDetails) {
+    return postService.createpost(postDetails);
+  }
 }
